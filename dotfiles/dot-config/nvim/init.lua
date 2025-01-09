d8ad2f4a93b6c8798178401f48d85f423190edd0
raphael <7124317+Raphael67@users.noreply.github.com>
2025-01-09 19:11:30 +0100

    change the way obsidian plugin is loaded to be able to :so it and reload it to test obsidian config without quitting vim

--- dotfiles/dot-config/nvim/init.lua
+++ dotfiles/dot-config/nvim/init.lua
@@ -45,7 +45,25 @@ require("lazy").setup({
   require("plugins.chatgpt"),
   require("plugins.aerial"),
   require("plugins.vim-tmux-navigator"),
-  require("plugins.obsidian"),
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    -- ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      "BufReadPre /Users/raphael/Library/Mobile Documents/iCloud~md~obsidian/Documents/*.md",
+      "BufNewFile /Users/raphael/Library/Mobile Documents/iCloud~md~obsidian/Documents/*.md",
+    },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+  },
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
@@ -92,3 +110,5 @@ end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+require("plugins.obsidian")

7bf4068862cdfa81260dae04e699cfa36b83f723
MEGABUGZ\rapha <7124317+Raphael67@users.noreply.github.com>
2025-01-12 10:54:47 +0100

    init arch setup script

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+#!/usr/sbin/bash
 
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-
 pacman -Syu
 
-pacman -Sy --noconfirm zsh cmake stow starship fzf gcc ripgrep tmux zlib eza zoxide lazygit wezterm fastfetch fd wget luarocks go
+pacman -Sy --noconfirm zsh cmake nodejs python stow starship fzf gcc ripgrep tmux zlib eza zoxide lazygit wezterm fastfetch fd wget luarocks go lua51 neovim
+
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+[multilib]
+/etc/pacma.d/mirrorlist
+EOT
+
+yay -S nvidia-dkms nvidia-utils lib32-nvidia-utils
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -14,3 +25,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache
 
 stow .
+
+if ! command -v rustup /dev/null 2>&1; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source ~/.cargo/env
+fi

f65e29f13294d671cdec834117601c873b45d71e
MEGABUGZ\rapha <7124317+Raphael67@users.noreply.github.com>
2025-06-26 12:06:51 +0200

    add a setup for arch on wsl

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,32 +1,48 @@
 #!/usr/sbin/bash
 
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+# Configure locales
+if ! grep -q "^en_US.UTF-8 UTF-8" /etc/locale.gen; then
+    echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+    echo "fr_FR.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
+    sudo locale-gen
+    echo "LANG=fr_FR.UTF-8" | sudo tee /etc/locale.conf
+fi
 
-pacman -Syu
+sudo pacman -Syu
 
-pacman -Sy --noconfirm zsh cmake nodejs python stow starship fzf gcc ripgrep tmux zlib eza zoxide lazygit wezterm fastfetch fd wget luarocks go lua51 neovim
+sudo pacman -Sy --noconfirm zsh cmake nodejs npm python stow gcc tmux zlib wezterm fastfetch wget luarocks go lua51 neovim fd fzf lazygit
 
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-sudo tee -a /etc/pacman.conf >/dev/null <<EOT
+# Add multilib repository if not already present
+if ! grep -q "^\[multilib\]" /etc/pacman.conf; then
+    sudo tee -a /etc/pacman.conf >/dev/null <<EOT
 [multilib]
-/etc/pacma.d/mirrorlist
+Include = /etc/pacman.d/mirrorlist
 EOT
+fi
 
 yay -S nvidia-dkms nvidia-utils lib32-nvidia-utils
 
+rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/mroth/evalcache ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/evalcache
 
+# Change default shell to zsh
+chsh -s /usr/bin/zsh
+
 stow .
 
-if ! command -v rustup /dev/null 2>&1; then
+if ! command -v rustup >/dev/null 2>&1; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     source ~/.cargo/env
 fi
+
+cargo install starship eza zoxide ripgrep
+
+sudo npm install -g @anthropic-ai/claude-code
